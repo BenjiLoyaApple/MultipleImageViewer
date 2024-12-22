de26b2ef65751047ceac47d5574cdf3be7040639
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-12-22 12:06:55 +0500

    Initial Commit

---
+++ MultipleImageViewer/MultipleImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleImageViewerApp.swift
+//  MultipleImageViewer
+//
+//  Created by Benji Loya on 22.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultipleImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
